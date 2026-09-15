d7a8a90872eebebeea4587059ff2ba4e86f19ee4
sssimmortal88-arch <sss.immortal88@gmail.com>
2026-09-15 23:06:59 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -259,3 +259,4 @@ app.post("/api/mark-delivered/:id", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+insert.run("Prismatic", 220, 1, "godly", "prismatic.webp"); //
